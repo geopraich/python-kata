a0fc7576f8a6105100790a11e22cd77e218a36ef
geopraich <sdd17@linuxmail.org>
2021-07-28 15:34:12 +0100

    simple refactor

--- python-katas-hackerrank/topic-algorithms/compare-the-triplets/comparethetriplets.py
+++ python-katas-hackerrank/topic-algorithms/compare-the-triplets/comparethetriplets.py
@@ -1,12 +1,12 @@
-def compare_triplets(a, b):
-    bscore = 0
-    ascore = 0
-    for numina, numinb in zip(a, b):
-        if numina == numinb:
+def compare_triplets(a, b,):
+    b_score = 0
+    a_score = 0
+    for num_in_a, num_in_b in zip(a, b):
+        if num_in_a == num_in_b:
             pass
-        elif numina < numinb:
-            bscore += 1
+        elif num_in_a < num_in_b:
+            b_score += 1
         else:
-            ascore += 1
-    resultlist = [ascore, bscore]
-    return resultlist
+            a_score += 1
+    result_list = [a_score, b_score]
+    return result_list
