a889e1f99cdf26530cf4c923ddeb2b5e263e563a
geopraich <sdd17@linuxmail.org>
2021-08-08 22:43:47 +0100

    new challenge

---
+++ python_katas_hackerrank/topic_python/find_runnerup/findrunnerup_test.py
@@ -0,0 +1,20 @@
+import unittest
+from findrunnerup import find_runner_up
+
+class MyTestCase(unittest.TestCase):
+    def test_find_runner_up(self):
+        result = find_runner_up([2,3,6,6,5])
+        self.assertEqual(result, 5, f"expected 5: actual {result}")
+
+class MyTestCase2(unittest.TestCase):
+    def test_find_runner_up(self):
+        result = find_runner_up([-10,-3,-6,-1,-5])
+        self.assertEqual(result, -3, f"expected -3: actual {result}")
+
+class MyTestCase3(unittest.TestCase):
+    def test_find_runner_up(self):
+        result = find_runner_up([-10, 100, 3, 45 ,-5, 45])
+        self.assertEqual(result, 45, f"expected 45: actual {result}")
+
+if __name__ == '__main__':
+    unittest.main()
