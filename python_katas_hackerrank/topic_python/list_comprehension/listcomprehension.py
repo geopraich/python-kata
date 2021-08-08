5491072d19360dc5bd586321ea5e61b3f3779cae
geopraich <sdd17@linuxmail.org>
2021-08-08 23:00:14 +0100

    added challenge

---
+++ python_katas_hackerrank/topic_python/list_comprehension/listcomprehension.py
@@ -0,0 +1,3 @@
+def list_comprehension_function(x,y,z,n):
+    final_lst = [[numx,numy,numz] for numx in range(0,x+1) for numy in range(0,y+1) for numz in range (0,z+1) if numx+numy+numz != n]
+    return final_lst
