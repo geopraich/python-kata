30d6c97927ae72f552c4a7ed52ca32f1a85cf7ef
geopraich <sdd17@linuxmail.org>
2021-08-10 14:30:37 +0100

    refactor

--- hackerrank/topic_python/find_percentage/findpercentagetest.py
+++ hackerrank/topic_python/find_percentage/findpercentagetest.py
@@ -6,6 +6,11 @@ class FindPercentageTestCase1(unittest.TestCase):
         result = find_percentage({"Krishna":[67,68,69], "Arjun":[70,98,63], "Malika":[52,56,60]}, "Malika")
         self.assertEqual(result, format(56.00, ".02f"), f"expected {56.00:.02f}: actual {result}")
 
+class FindPercentageTestCase2(unittest.TestCase):
+    def test_find_percentage(self):
+        result = find_percentage({"Krishna":[67,68,69], "Arjun":[70,98,63], "Malika":[52,56,60]}, "Arjun")
+        self.assertEqual(result, format(77.00, ".02f"), f"expected {77.00:.02f}: actual {result}")
+
 
 if __name__ == '__main__':
     unittest.main()
